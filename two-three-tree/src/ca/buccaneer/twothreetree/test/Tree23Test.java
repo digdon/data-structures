0115cf8e676c71364fa8aee76cc79aa1f29dee9c
Mike Digdon <32070772+digdon@users.noreply.github.com>
2025-10-30 15:55:51 -0300

    Removing some commented code and unused imports

--- two-three-tree/src/ca/buccaneer/twothreetree/test/Tree23Test.java
+++ two-three-tree/src/ca/buccaneer/twothreetree/test/Tree23Test.java
@@ -1,19 +1,11 @@
 package ca.buccaneer.twothreetree.test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import ca.buccaneer.twothreetree.TwoThreeTree;
 
 public class Tree23Test {
 
     public static void main(String[] args) {
         TwoThreeTree<Integer> tree = new TwoThreeTree<>(null);
-
-//        Integer[] values = new Integer[1000];
-//        for (int i = 0; i < values.length; i++) {
-//            values[i] = i;
-//        }
 
         for (int i = 0; i < 100; i++) {
             System.out.print("adding " + i + "...");
